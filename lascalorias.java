4f872f2b36ecedd16d9682527126eaee8af9cc55
DaniloHer1 <clasec2comercio@gmail.com>
2024-12-17 19:21:01 +0100

    Add files via upload

---
+++ lascalorias.java
@@ -0,0 +1,42 @@
+
+public class lascalorias {
+	static java.util.Scanner entrada;
+
+	public static void main(String args[]) {
+		entrada = new java.util.Scanner(System.in);
+		while (casoDePrueba()) {
+		}
+	}
+
+	private static boolean casoDePrueba() {
+
+	
+		
+		int calorias=entrada.nextInt();
+		
+		if (calorias == 0) {
+			return false;
+		}
+
+		int numerocomidas = entrada.nextInt();
+		int caloriastotal;
+		double sumtotal = 0;
+		int cont = 0;
+
+		
+		while (numerocomidas != cont){
+			
+			caloriastotal = entrada.nextInt();
+			sumtotal+=caloriastotal;
+			cont++;
+
+		} 
+					
+						
+		int resultado= (int) (Math.ceil(sumtotal/calorias)*calorias);
+		
+		System.out.println(resultado/calorias);
+
+		return true;
+	}
+}
